4d2b0fe29a9556c0d8fcaa97fe3aa7b9f05b62db
Wesley Lizarti <wesley.lizarti@gmail.com>
2020-05-30 05:57:06 -0300

    Colocando url do ws no código porque netlify

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const yandexAPIUrl = process.env.VUE_APP_YANDEX_TRANSLATE_URL
 export const yandexAPIKey = process.env.VUE_APP_YANDEX_TRANSLATE_KEY
-export const wsBaseUrl = process.env.VUE_APP_WS_URL
+export const wsBaseUrl = 'https://lizarti.ml/chat-cosmopolitano'
